f3b3cfbe7434f29b000d85046fa49f66c18420a4
618knot <publicac.clt618knot@gmail.com>
2024-02-24 22:49:50 +0900

    commit

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Cookie, Response
 from starlette.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from starlette.requests import Request
 from dotenv import load_dotenv
+import time
 import os
 import redis
+import hashlib
+
+from models.sqlite.util import session
+from models.user import User
 
 load_dotenv()
 REDIS_HOST = os.environ.get("REDIS_HOST")
@@ -26,24 +31,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("cart.html", {"request": request})
+
+
 class Cart(BaseModel):
     item01: Optional[int]
     item02: Optional[int]
     item03: Optional[int]
 
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse("cart.html", {"request": request})
-
 @app.post("/api/cart", response_class=HTMLResponse)
-async def cart(request: Cart):
+async def cart(cart: Cart, request: Request):
     cart_dict = {
-        "item01": request.item01,
-        "item02": request.item02,
-        "item03": request.item03
+        "item01": cart.item01,
+        "item02": cart.item02,
+        "item03": cart.item03
     }
-
-    red.hmset("hoge", cart_dict)
 
     msg = """
     <h4>保存された値</h4>
@@ -62,4 +66,81 @@ async def cart(request: Cart):
         idx += 1
 
     msg += "</table>"
-    return msg
+    response = Response(content=msg)
+
+    cookie = request.cookies.get("_session")
+    if cookie == None:
+        cookie = str(time.time())
+        response.set_cookie(key="_session", value=cookie, expires=600)
+
+    # redisにカートを保存
+    red.hmset(cookie, cart_dict)
+
+    return response
+
+@app.get("/api/cart")
+async def get_cart(request: Request):
+    _session = request.cookies.get("_session")
+    if _session:
+        return red.hmget(_session)
+    return "なし"
+
+@app.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    _session = request.cookies.get("_session")
+    if is_sign_in(_session):
+        return RedirectResponse(url="/mycart")
+    return templates.TemplateResponse("register.html", {"request": request})
+
+class UserReq(BaseModel):
+    email: str
+    password: str
+
+@app.post("/api/register")
+async def register(request: UserReq):
+    try:
+        _session = str(time.time())
+        encrypted_password = hashlib.sha256(request.password.encode("utf-8")).hexdigest()
+        user = User(email=request.email, password=encrypted_password, session=_session)
+        session.add(user)
+        session.commit()
+        response = Response()
+        response.set_cookie(key="_session", value=_session, expires=600)
+        return response
+    except:
+        session.rollback()
+        return Response(status_code=400)
+
+@app.get("/login")
+async def login(request: Request):
+    _session = request.cookies.get("_session")
+
+    if is_sign_in(_session):
+        return RedirectResponse(url="/mycart")
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/api/login")
+async def login(request: UserReq):
+    _session = str(time.time())
+    encrypted_password = hashlib.sha256(request.password.encode("utf-8")).hexdigest()
+    user = session.query(User).filter(request.email == User.email, encrypted_password == User.password).first()
+
+    if user:
+        user.session = _session
+        session.commit()
+        response = Response()
+        response.set_cookie(key="_session", value=_session, expires=600)
+        return response
+    
+    return RedirectResponse(url="/register")
+
+@app.get("/mycart", response_class=HTMLResponse)
+async def mycart(request: Request):
+    _session = request.cookies.get("_session")
+
+    if not is_sign_in(_session):
+        return RedirectResponse(url="/login")
+    return templates.TemplateResponse("cart.html", {"request": request})
+
+def is_sign_in(_session: str) -> bool:
+    return session and session.query(User).filter(_session == User.session).count()
---
+++ app/models/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+print(DB_NAME)
+
+engine = create_engine(f"sqlite:///{DB_NAME}", echo=True)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    session = Column(String)
+
+Base.metadata.create_all(bind=engine)
